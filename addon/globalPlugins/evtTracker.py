019cfbc1fe4ddc4fbe2946a12004fb0fb2cd0010
Joseph Lee <joseph.lee22590@gmail.com>
2022-05-20 14:46:32 -0700

    Global plugin: update copyright year

--- addon/globalPlugins/evtTracker.py
+++ addon/globalPlugins/evtTracker.py
@@ -1,5 +1,5 @@
 # NVDA Event Tracker/Investigator
-# Copyright 2017-2021 Joseph Lee, released under GPL.
+# Copyright 2017-2022 Joseph Lee, released under GPL.
 
 from comtypes import COMError
 import globalPluginHandler
