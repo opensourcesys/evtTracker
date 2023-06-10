7afcadb6eaaada12cac17fb13aeda4a29dcb192d
Luke Davis <XLTechie+github@newanswertech.com>
2023-06-10 03:40:35 -0400

    Version bump for next build. Author and URL update. Readme.md filename lowercasing.

--- buildVars.py
+++ buildVars.py
@@ -25,13 +25,13 @@ addon_info = {
 	# Translators: Long description to be shown for this add-on on add-on information from add-ons manager
 	"addon_description": _("""Tracks events and displays information about them"""),
 	# version
-	"addon_version": "23.04",
+	"addon_version": "23.5.0",
 	# Author(s)
-	"addon_author": "Joseph Lee <joseph.lee22590@gmail.com>, Thiago Seus <thiago.seus@yahoo.com.br>",
+	"addon_author": "Joseph Lee <joseph.lee22590@gmail.com>, Luke Davis <XLTechie@newanswertech.com>, Thiago Seus <thiago.seus@yahoo.com.br>",
 	# URL for the add-on documentation support
 	"addon_url": "https://addons.nvda-project.org/",
 	# URL for the add-on repository where the source code can be found
-	"addon_sourceURL": "https://github.com/josephsl/evtTracker",
+	"addon_sourceURL": "https://github.com/opensourcesys/evtTracker",
 	# Documentation file name
 	"addon_docFileName": "readme.html",
 	# Minimum NVDA version supported (e.g. "2018.3.0", minor version is optional)
